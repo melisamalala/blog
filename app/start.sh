387a01b5ed5bb8865ff287db0ef8488412a68823
melisamalala <melissamalala@gmail.com>
2018-09-12 15:43:14 +0300

    Created SPECS.md file

---
+++ app/start.sh
@@ -0,0 +1,8 @@
+export SECRET_KEY=abc123
+
+export MAIL_USERNAME=moringamelissa254@gmail.com
+export MAIL_PASSWORD=moringa123
+
+export DATABASE_URL='postgresql+psycopg2://melissamalala:abc123@localhost/blog'
+
+python3.6 manage.py server
