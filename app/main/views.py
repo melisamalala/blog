9040c1182c4dc1d920886fd7d1098d31682c6e8c
melisamalala <melissamalala@gmail.com>
2018-09-13 10:09:47 +0300

    Edited the views file on main folder

---
+++ app/main/views.py
@@ -0,0 +1,199 @@
+from flask import render_template, request, redirect,url_for, abort, flash
+from . import main
+from .forms import BlogpostForm, UpdateProfile, CommentForm
+from ..models import Blogpost, User, Comment
+from flask_login import login_required, current_user
+from .. import db, photos
+import markdown2
+
+# INDEX PAGE
+@main.route('/')
+def index():
+    """ View root page function that returns index page """
+
+    # category = Category.get_categories()
+    blogposts = Blogpost.query.all()
+
+    title = 'WELCOME TO BLOGPOST'
+    return render_template('index.html', title = title, blogposts=blogposts)
+
+# VIEWING EACH SPECIFIC PROFILE
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+# UPDATING PROFILE
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+# UPDATING PICTURE
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+# ADDING A NEW BLOGPOST
+@main.route('/blogpost/new', methods=['GET','POST'])
+@login_required
+def new_blogpost():
+    form = BlogpostForm()
+
+    if form.validate_on_submit():
+
+        title=form.title.data
+        content=form.content.data
+        category_id=form.category_id.data
+        blogpost = Blogpost(title=title,
+                      content=content,
+                      category_id=category_id,
+                      user=current_user)
+
+        db.session.add(blogpost)
+        db.session.commit()
+
+        # blogpost.save_blogpost(blogpost)
+        print('kasambuli')
+        flash('Your blogpost has been created!', 'success')
+        return redirect(url_for('main.single_blogpost',id=blogpost.id))
+
+    return render_template('newblogpost.html', title='New Post', blogpost_form=form, legend='New Post')
+
+# VIEW INDIVIDUAL BLOGPOST
+@main.route('/blogpost/new/<int:id>')
+def single_blogpost(id):
+    blogpost = Blogpost.query.get(id)
+    return render_template('singleblogpost.html',blogpost = blogpost)
+
+@main.route('/allblogposts')
+def blogpost_list():
+    # Function that renders the business category blogpost and its content
+
+    blogpostS = Blogpost.query.all()
+
+    return render_template('blogposts.html', blogposts=blogposts)
+
+
+# VIEWING A blogpost WITH COMMENTS AND COMMENTFORM
+@main.route('/blogpost/<int:blogpost_id>/',methods=["GET","POST"])
+def blogpost(blogpost_id):
+    blogpost = Blogpost.query.get(blogpost_id)
+    form = CommentForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        comment = form.comment.data
+        new_blogpost_comment = Comment(post_comment=comment,
+                                       blogposts=blogpost_id,
+
+                                    # user_id=current_user.id,
+                                    user=current_user)
+        # new_post_comment.save_post_comments()
+
+
+        db.session.add(new_blogpost_comment)
+        db.session.commit()
+    comments = Comment.query.all()
+    return render_template('blogpostlink.html', title=blogpost.title,
+                           blogpost=blogpost,
+                           blogpost_form=form,
+                           comments=comments)
+
+# ADDING A NEW COMMENT TO A blogpost
+@main.route('/blogpost/comment/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_comment(id):
+    '''
+    view category that returns a form to create a new comment
+    '''
+    form = CommentForm()
+    blogpost = Blogpost.query.filter_by(id = id).first()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        comment = form.comment.data
+
+        # comment instance
+        new_comment = Comment(blogpost_id = blogpost.id,
+                              post_comment = comment,
+                              title=title,
+                              user = current_user)
+
+        # save comment
+        new_comment.save_comment()
+
+        return redirect(url_for('.blogposts', id = blogpost.id ))
+
+    title = f'{blogpost.title} comment'
+    return render_template('newcomment.html', title = title, comment_form = form, blogpost = blogpost, )
+
+# UPDATING A blogpost
+
+@main.route('/blogpost/<int:blogpost_id>/update', methods=['GET','POST'])
+@login_required
+def update_blogpost(blogpost_id):
+    blogpost = Blogpost.query.get_or_404(blogpost_id)
+    if blogpost.author != current_user:
+        abort(403)
+    form = BlogpostForm()
+    if form.validate_on_submit():
+        blogpost.title = form.title.data
+        blogpost.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('main.blogpostlink', blogpost_id=blogpost.id))
+    elif request.method == 'GET':
+        form.title.data = blogpost.title
+        form.content.data = blogpost.content
+    return render_template('newblogpost.html', title='Update blogpost', form=form, legend='Update blogpost')
+
+# DELETING A blogpost
+@main.route('/blogpost/<int:blogpost_id>/delete', methods=['GET', 'POST'])
+@login_required
+def delete_blogpost(blogpost_id):
+    blogpost = Blogpost.query.get_or_404(blogpost_id)
+    for comment in blogpost.comments.all():
+        db.session.delete(comment)
+        db.session.commit()
+    if blogpost.author != current_user:
+        abort(403)
+    db.session.delete(blogpost)
+    db.session.commit()
+    flash('Your blogpost has been deleted!', 'success')
+    return redirect(url_for('main.blogposts'))
+
+# VIEWING A SPECIFIC blogpost
+@main.route("/view/<id>", methods=["GET","POST"])
+def view_blogpost(id):
+    blogpost = Blogpost.query.get(id)
+    if request.args.get("vote"):
+       blogpost.likes = blogpost.likes + 1
+       blogpost.save_blogpost()
+       return redirect("/view/{blogpost_id}".format(blogpost_id=id))
+    return render_template('view_blogpost.html',blogpost = blogpost)
