c500b02a61c77032dc1a712cd7c96fc9419bfd59
melisamalala <melissamalala@gmail.com>
2018-09-17 09:42:17 +0300

    heroku deploy try

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://melissamalala:abc123@localhost/blog'
+    # SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://melissamalala:abc123@localhost/blog'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
